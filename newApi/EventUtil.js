fbc8df940ae8c915927fddb22a4960fa02ecf692
zhgf <578663379@qq.com>
2016-12-27 09:53:21 +0800

    1

---
+++ newApi/EventUtil.js
@@ -0,0 +1,27 @@
+var EventUtil = {
+	addHandler: function(element, type, handler){
+		if(element.addEventListener){
+			element.addEventListener(type, handler, false);
+		} else if(element.attachEvent){
+			element.attachEvent("on" + type, handler);
+		} else {
+			element["on" + type] = handler;
+		}
+	},
+	removeHandler: function(element, type, handler){
+		if (element.removeEventListener){
+			element.removeEventListener(type, handler, false);
+		} else if(element.detachEvent){
+			element.detachEvent("on" + type, handler);
+		} else{
+			element["on" + type] = null;
+		}
+	}
+}
+
+// var btn1 = document.getElementById("myBtn1");
+// var handler = function(){
+// 	alert("hello handler");
+// }
+// EventUtil.addHandler(btn1, "click", handler); 
+//EventUtil.removeHandler(btn1, "click", handler);
